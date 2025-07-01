db0111943012731a98bd0d25fd85224909cb5f58
NikaBarbaqadze <nikavip17@gmail.com>
2025-07-01 23:58:36 +0400

    Update ConnectionDB.java

--- src/chack/ConnectionDB.java
+++ src/chack/ConnectionDB.java
@@ -4,9 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/**
- * Handles creating connections to the database.
- */
 public class ConnectionDb {
     private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=ToDo;"
             + "encrypt=true;"
@@ -14,11 +11,6 @@ public class ConnectionDb {
     private static final String USER = "test_user";
     private static final String PASSWORD = "1234";
 
-    /**
-     * Get a new connection to the database.
-     * @return Connection object
-     * @throws SQLException if the connection fails
-     */
     public static Connection getConnection() throws SQLException {
         Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
         System.out.println("Connected successfully");
